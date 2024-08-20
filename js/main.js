3e97d34fd61109aa25af8243055df0a016a279f4
Tien <ngotienpts@gmail.com>
2024-08-20 17:22:06 +0700

    20/8

---
+++ js/main.js
@@ -0,0 +1,184 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Tập hợp tất cả các phần tử cần sử dụng
+    const backTop = document.querySelector("#back-top");
+    // slide
+    const twoSlidesTwoRow = document.querySelectorAll(".js__twoSlidesTwoRowContainer");
+
+    const stickyHeaderPC = document.querySelector(".js__stickyHeader");
+    const video169s = document.querySelectorAll(".js__video169");
+    const fancyboxes = document.querySelectorAll(".fancybox-full");
+
+    // show sub menu
+    const dropdownSubMenu = document.querySelectorAll(".js__dropDown");
+    const subMenu = document.querySelector(".js__clickShowSubMenu");
+
+
+    // sticky
+    const stickyContainers = document.querySelectorAll('.js__stickyContainer')
+
+    // Xử lý sự kiện khi nhấn nút "back to top"
+    function handleBackTop() {
+        if (!backTop) return;
+
+        backTop.onclick = function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        };
+
+    }
+
+    // xử lý sự kiện để show sub menu
+    function handleShowSubMenu() {
+        if (!subMenu) return;
+        var closeSubMenu = document.querySelector(".js__closeSubMenu");
+        var overlay = document.querySelector(".js__overlay");
+        var parentBox = subMenu.parentElement;
+
+        subMenu.onclick = function () {
+            this.parentElement.classList.add("active");
+            document.querySelector("body").style.overflow = "hidden";
+        };
+        closeSubMenu.onclick = function () {
+            parentBox.classList.remove("active");
+            document.querySelector("body").style.overflow = "auto";
+        };
+        overlay.onclick = function () {
+            parentBox.classList.remove("active");
+            document.querySelector("body").style.overflow = "auto";
+        };
+    }
+
+    // Xử lý sự kiện để show dropdown submenu
+    function handleShowDropdownSubMenu() {
+        dropdownSubMenu &&
+            dropdownSubMenu.forEach((item) => {
+                var parent = item.parentElement;
+                var nextEle = parent.querySelector(".js__listSubMenu");
+                item.onclick = function () {
+                    parent.classList.toggle("active");
+                    if (nextEle.style.maxHeight) {
+                        nextEle.style.maxHeight = null;
+                    } else {
+                        nextEle.style.maxHeight = nextEle.scrollHeight + "px";
+                    }
+                };
+            });
+    }
+
+
+    // Xử lý video tỉ lệ 16:9
+    function handleVideo169() {
+        if (video169s) {
+            video169s.forEach((video169) => {
+                var videos = video169.querySelectorAll("iframe");
+                if (videos) {
+                    videos.forEach((video) => {
+                        var w = video.offsetWidth;
+                        video.style.height = (w * 9) / 16 + "px";
+                    });
+                }
+            });
+        }
+    }
+
+    // Khởi tạo fancybox
+    function initFancybox() {
+        if (fancyboxes) {
+            fancyboxes.forEach(function () {
+                $(".fancybox-full a").fancybox();
+            });
+        }
+    }
+
+    // Khởi tạo sticky content 
+    function initStickyContent() {
+        if (!stickyContainers) return;
+
+        stickyContainers.forEach(item => {
+            var stickyElements = [item.querySelector('.js__stickyLeft'), item.querySelector('.js__stickyRight')]
+                .filter(element => element !== null);
+
+            stickyElements.forEach(element => {
+                $(element).theiaStickySidebar({
+                    additionalMarginTop: 60,
+                });
+            });
+        });
+    }
+
+    // Khởi tạo slider với một 2 item 2 hàng
+    function initSliderTwoItemsTwoRow() {
+        if (twoSlidesTwoRow) {
+            twoSlidesTwoRow.forEach((item) => {
+                var slider = item.querySelector(".js__twoSlide");
+                var next = item.querySelector(".swiper-button-next");
+                var prev = item.querySelector(".swiper-button-prev");
+                var pagi = item.querySelector(".swiper-pagination");
+
+                new Swiper(slider, {
+                    slidesPerView: 2,
+                    grid: {
+                        rows: 2,
+                        fill: "row",
+                    },
+                    spaceBetween: 40,
+                    navigation: {
+                        nextEl: next || null,
+                        prevEl: prev || null,
+                    },
+                    pagination: {
+                        el: pagi || null,
+                        clickable: true,
+                    },
+                });
+            });
+        }
+    }
+
+    // Xử lý thanh header dính
+    function handleStickyHeader() {
+        if (stickyHeaderPC) {
+            const isSticky = scrollY > 300;
+            stickyHeaderPC.classList.toggle("sticky", isSticky);
+        }
+    }
+    // Hàm hiển thị nút backTop dựa trên vị trí cuộn trang
+    function handleBackTopVisibility() {
+        if (backTop) {
+            if (
+                document.body.scrollTop > 300 ||
+                document.documentElement.scrollTop > 300
+            ) {
+                backTop.style.opacity = 1;
+                backTop.style.visibility = "visible";
+            } else {
+                backTop.style.opacity = 0;
+                backTop.style.visibility = "hidden";
+            }
+        }
+    }
+    // Xử lý sự kiện khi cuộn trang
+    function handleWindowScroll() {
+        window.onscroll = function () {
+            handleStickyHeader();
+            handleBackTopVisibility()
+        };
+    }
+
+    // Khởi tạo tất cả các chức năng
+    function initApp() {
+        handleBackTop();
+        handleShowSubMenu();
+        handleShowDropdownSubMenu();
+        handleVideo169();
+        initFancybox();
+        initStickyContent();
+        // slide
+        initSliderTwoItemsTwoRow();
+        // scroll
+        handleWindowScroll();
+    }
+
+    // Bắt đầu khởi tạo ứng dụng
+    initApp();
+});
